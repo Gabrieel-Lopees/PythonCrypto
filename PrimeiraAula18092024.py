56343548d98b99ecb561452f93ee6943d5f8c9d4
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-09-18 13:28:20 -0300

    exercicio 3

--- PrimeiraAula18092024.py
+++ PrimeiraAula18092024.py
@@ -9,9 +9,9 @@ print ("Olá, ", username)
 
 print (x, y, z) '''
 
-print("Olá Mundo")
+'''print("Olá Mundo")'''
 
-x = int(input ("Variavel um:"))
+'''x = int(input ("Variavel um:"))
 y = int(input ("Variavel dois: "))
 
 if x > y:
@@ -20,4 +20,18 @@ elif y == x:
     print("São iguais!")
     
 else:
-    print("Y é maior que X!")
+    print("Y é maior que X!")'''
+    
+x = int(input ("Variavel X:"))
+y = int(input ("Variavel Y: "))
+z = int(input ("Variavel Z: "))
+
+#verificar se é Z
+if x > z and y > z:
+    print("Z é a menor")
+#verificar se é X
+elif z > x and y > x: 
+    print("X é a menor")
+#verificar se é Y
+else:
+    print("Y é a menor")
