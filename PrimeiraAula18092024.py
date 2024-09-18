0485920d05134666b439c280e6a37dd3b901ed91
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-09-18 13:19:30 -0300

    HWSHADJKAJKD

--- PrimeiraAula18092024.py
+++ PrimeiraAula18092024.py
@@ -1,7 +1,23 @@
-#Primeira aula pratica de python em cryptografia 
+'''Primeira aula pratica de python em cryptografia 
 
 x = "pyhton"
 y = "é"
 z = "legal"
 
-print (x, y, z)
+username = input ("Seu nome: ")
+print ("Olá, ", username)
+
+print (x, y, z)'''
+
+print("Olá Mundo")
+
+x = input ("Variavel um:")
+y = input ("Variavel dois: ")
+
+if x > y:
+    print("X é maior que Y!")
+elif y == x: 
+    print("São iguais!")
+    
+else:
+    print("Y é maior que X!")
