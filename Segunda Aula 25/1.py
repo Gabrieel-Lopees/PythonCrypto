7e14e8c6d3d28a806b6c13657ae678a2a876f031
Gabriel Lopes <gabriel-lopes-universidade@proton.me>
2024-09-25 15:22:43 -0300

    yes

--- Segunda Aula 25/1.py
+++ Segunda Aula 25/1.py
@@ -1,36 +1,32 @@
 import os
 
 arqNome = input("Informe o nome do arquivo: ")
-arq = open(arqNome,'r')
+arq = open(arqNome, 'r+')
 
-print("Escolha essas opções")
-print("1 exiba o seu conteúdo na tela")
-print("2 acrescente a palavra “Final” no fim do arquivo")
-print("3 deixe este arquivo sem conteúdo algum")
-print("4 mudar nome do arquivo")
-print("5 Encerrar programa")
+while True:
+    print("Escolha uma das opções:")
+    print("1 - Exibir o seu conteúdo na tela")
+    print("2 - Acrescentar a palavra 'Final' no fim do arquivo")
+    print("3 - Deixar este arquivo sem conteúdo algum")
 
+    escolha = int(input("Digite sua escolha: "))
 
-escolha = int(input())
+    if escolha == 1:
+        arq.seek(0)
+        words = [word.strip() for word in arq]
+        print(words)
+    elif escolha == 2:
+        arq.seek(0, os.SEEK_END)
+        arq.write("Final\n")
+        arq = open(arqNome, 'r+')
+    elif escolha == 3:
+        arq.truncate()
+        arq = open(arqNome, 'r+')
+    else:
+        print("entrada errada")
 
+    continuar = input("continuar? (s/n): ")
+    if continuar.lower() != 's':
+        break
 
-if escolha == 1:
-    words = [word.split(':')[0] for word in arq]
-    print(words)
-elif escolha == 2:
-    arq.close()
-    with open(arqNome, 'a') as arq_append:
-        arq_append.write("Final")
-elif escolha == 3:
-    arq.close()
-    with open(arqNome,'w') as arq_clear:
-        arq_clear.write("")
-elif escolha == 4:
-    novoNome = input("Informe o novo nome do arquivo: ")
-    import os
-
-    os.rename(arqNome, novoNome)  # Renomeia o arquivo
-    print(f"O arquivo foi renomeado para: {novoNome}")
-
-else:
-    print("Programa encerrado")
+arq.close()
