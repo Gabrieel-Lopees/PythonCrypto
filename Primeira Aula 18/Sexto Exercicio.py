75414010905471bc9a1c40ed0af49794de20514f
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-09-25 14:25:29 -0300

    exercicios

--- Primeira Aula 18/Sexto Exercicio.py
+++ Primeira Aula 18/Sexto Exercicio.py
@@ -1,7 +1,7 @@
 num = int(input ("Escreva uma numero e dirá se é par ou n: "))
 
 if num % 2 == 0:
-    print("Seu numero {num} é par")
+    print(f"Seu numero {num} é par")
     
 else:
-    print("Seu numero {num} é impar")
+    print(f"Seu numero {num} é impar")
---
+++ Segunda Aula 25/Segundo Exercicio.py
@@ -0,0 +1,8 @@
+Numeros = []
+
+numero = input("Digite um conjunto de valores numericos para serem exibidos em ordem crescente:")
+
+while numero == 999:
+    break
+Numeros.append(numero)
+print(Numeros)
