c1d35894240b653494c04e0a399d5746fb71bb14
Gabriel Lopes <gabriel-lopes-universidade@proton.me>
2024-12-04 14:15:02 -0300

    chaves

--- Sexta Aula 04/GabrielLopes_Exerc_1.py
+++ Sexta Aula 04/GabrielLopes_Exerc_1.py
@@ -1,32 +1,32 @@
-from Crypto.PunlicKey import RSA
+from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import os
 
-#Gerando as chaves 
 def gerar_chaves():
-    chave = RSA.generate (2048) 
-    chave_privada = chave.export_key()
-    chave_publica = chave.publickey().export_key()
+    chave = RSA.generate(2048)  
+    chave_privada = chave.export_key()  
+    chave_publica = chave.publickey().export_key() 
     
     with open("chave_privada.pem", "wb") as f_privada:
         f_privada.write(chave_privada)
     
     with open("chave_publica.pem", "wb") as f_publica:
         f_publica.write(chave_publica)
-        
+
+def criptografar(mensagem):
     with open("chave_publica.pem", "rb") as f_publica:
         chave_publica = RSA.import_key(f_publica.read())
-
+    
     cifra = PKCS1_OAEP.new(chave_publica)
-    mensagem_encriptada = cifra.encrypt(mensagem.encode())
+    mensagem_encriptada = cifra.encrypt(mensagem.encode()) 
     return mensagem_encriptada
 
 def descriptografar(mensagem_encriptada):
     with open("chave_privada.pem", "rb") as f_privada:
         chave_privada = RSA.import_key(f_privada.read())
-        
+    
     cifra = PKCS1_OAEP.new(chave_privada)
-    mensagem_descriptografada = cifra.decrypt(mensagem_encriptada).decode()
+    mensagem_descriptografada = cifra.decrypt(mensagem_encriptada).decode() 
     return mensagem_descriptografada
 
 def main():
@@ -34,11 +34,11 @@ def main():
     
     mensagem = input("Digite a mensagem a ser criptografada: ")
     
-    mensagem_encriptada - criptografar(mensagem)
+    mensagem_encriptada = criptografar(mensagem)
     print("Mensagem criptografada: ", mensagem_encriptada)
     
     mensagem_descriptografada = descriptografar(mensagem_encriptada)
-    print("Mensagem descriptofrada: ", mensagem_descriptografada)
+    print("Mensagem descriptografada: ", mensagem_descriptografada)
     
 if __name__ == "__main__":
     main()
