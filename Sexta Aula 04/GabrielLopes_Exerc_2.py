a070638f6b4bc0f08d527f0edec50ebd1e3a9195
Gabriel Lopes <159876571+Gabrieel-Lopees@users.noreply.github.com>
2024-12-04 11:41:44 -0300

    Update GabrielLopes_Exerc_2.py

--- Sexta Aula 04/GabrielLopes_Exerc_2.py
+++ Sexta Aula 04/GabrielLopes_Exerc_2.py
@@ -1,29 +1,19 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-# Função para descriptografar a mensagem com a chave privada
 def descriptografar(mensagem_encriptada):
-    # Carregar a chave privada (que foi transferida do Computador A)
     with open("chave_privada.pem", "rb") as f_privada:
         chave_privada = RSA.import_key(f_privada.read())
     
-    # Descriptografar a mensagem com a chave privada
     cifra = PKCS1_OAEP.new(chave_privada)
-    mensagem_descriptografada = cifra.decrypt(mensagem_encriptada).decode()  # Convertendo bytes para string
+    mensagem_descriptografada = cifra.decrypt(mensagem_encriptada).decode()
     return mensagem_descriptografada
 
 def main():
-    # Passo 1: Carregar a mensagem criptografada (transferida do Computador A)
-    # Aqui você substituiria pelo conteúdo que recebeu, pode ser de um arquivo ou diretamente como bytes.
+    mensagem_encriptada = b's\xc0\x92z\xfaNIA\x04\xb0u\xe3\x96\xb1\xb8\xf4\x98\x92\x12\x11d\xf3\'\xe0\\\xf5\x7f\xdd\x18\xf2#hdU\x12\xd8j\xfd\x13$\x86\x08\x93\x03\xd0\x8bS\xd4h\xc4\xb1\xc7Lw\x16c\xdd\xdd\x9da\xafLc\xcf\x1e\xfc+\'\xdd\xc3\xe3\x0f\x11\xc3{<bC\x03\x93j\x1e\xbf\x96\xe9/\r\xeb\x18_M\xe8\xdfM\xe7\xcc>~-b\xd6\xe9}WiR\x8cc_\xdb\n\t/\xf9\x18\xdb\xe6\x84\xd9f\x88\xcf\xa6!\xa6\x02\xa9k\x9e(\x92\xbb0\x98\xba\xd6BQ^\'!\xca\xaa\xf4R\xb6\xc1]y\xb1v\x12\x88q\xa2\x16\x98\x15\xde\xf7R\xdbcIOy\xe6p\x9f\xe8\x8fcJ\x83f\xb9\xd1P\xce-\x99,\x03\x00aS\x0f\xcb@A:\xb5"\x85\xf9\xb5\xc9\x7f]&\\@W\xe8\xa00\x9d\x1e\xdc\x86\x03\xdd\xe6)\x0e\xa2F3\xe10X\xecw\xfa\xd6\xb7\xa9\x9b\x867c\x17\xab\xf7\xc1g\x85\xcb\xdd\x9b\xf9I__\xc4\xdc\xc6\x16 \x1d\xad\xe7\xdb\x9f\x1fi'  
     
-    # Exemplo: Digite ou copie a mensagem criptografada recebida de A (em formato binário)
-    # Exemplo de uma mensagem criptografada em bytes (substitua pela real mensagem recebida)
-    mensagem_encriptada = b'\x9b\xdf\xab\xd2\x7f\x92...'  # Exemplo: a mensagem criptografada real
-    
-    # Passo 2: Descriptografar a mensagem com a chave privada
     mensagem_descriptografada = descriptografar(mensagem_encriptada)
     
-    # Passo 3: Exibir a mensagem descriptografada
     print("Mensagem descriptografada: ", mensagem_descriptografada)
 
 if __name__ == "__main__":
