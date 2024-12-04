01e202ed733e2e1ae6809e95bd482e87065ee4e2
Gabriel Lopes <gabriel-lopes-universidade@proton.me>
2024-12-04 13:56:07 -0300

    Momento PYTHON

--- Primeira Aula 18/Terceiro Exercicio.py
+++ Primeira Aula 18/Terceiro Exercicio.py
@@ -1,10 +1,11 @@
-x = int(input ("Variavel X:"))
+x = int(input ("Variavel X: "))
 y = int(input ("Variavel Y: "))
 z = int(input ("Variavel Z: "))
 
 #verificar se é Z
 if x > z and y > z:
     print("Z é a menor")
+
 #verificar se é X
 elif z > x and y > x: 
     print("X é a menor")
